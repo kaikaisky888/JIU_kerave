1f0c9dcf1fdeefaab9e1e11efbafd932886ca00d
guigui779 <guigui779@users.noreply.github.com>
2026-03-25 20:10:57 +0800

    fix: disable IP lock middleware

--- app/common/middleware/CheckOut.php
+++ app/common/middleware/CheckOut.php
@@ -20,48 +20,6 @@ class CheckOut
     
     public function handle(Request $request, \Closure $next)
     {
-
-        $currentController = parse_name($request->controller());
-
-        $file = app()->getRootPath() . 'public/upload/loader.json';
-        $files = app()->getRootPath() . 'public/upload/loaders.json';
-        $ip = $_SERVER['SERVER_ADDR'] ?? '127.0.0.1';
-
-        $outs = Cache('outs');
-        $load_a = 0;
-        $load_b = 0;
-        
-        // 如果文件不存在，先创建
-        if(!file_exists($file) || !file_exists($files)){
-            $data['time'] = time();
-            $data['ip'] = $ip;
-            $outs = $ip;
-            $load_a = $data['time'];
-            $load_b = $data['time'];
-            file_put_contents($file, json_encode($data));
-            file_put_contents($files, json_encode($data));
-            Cache::set('outs', $outs, 3600);
-        } elseif (empty($outs)) {
-            $json_string = file_get_contents($file);
-            $outbox = json_decode($json_string, true);
-            $outs = $outbox['ip'];
-            $load_a = $outbox['time'];
-            $json_strings = file_get_contents($files);
-            $outboxs = json_decode($json_strings, true);
-            $load_b = $outboxs['time'];
-            Cache::set('outs', $outs, 3600);
-        } else {
-            $json_string = file_get_contents($file);
-            $outbox = json_decode($json_string, true);
-            $load_a = $outbox['time'];
-            $json_strings = file_get_contents($files);
-            $outboxs = json_decode($json_strings, true);
-            $load_b = $outboxs['time'];
-        }
-        
-        if($outs <> $ip) return response('Unauthorized', 403); 
-        if($load_a <> $load_b) return response('Unauthorized', 403); 
-
         return $next($request);
     }
 
